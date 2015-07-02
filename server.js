e49ca02f06135751abe10c95dd3f30a4718c4563
Patrick Chiang <pchiang@uw.edu>
2015-07-03 00:38:36 +0800

    Basic socket practice

--- server.js
+++ server.js
@@ -7,12 +7,47 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+
+/**
+ * User Management
+ */
+
+var localUser;
+
+/**
+ * User Definitions
+ */
+function User(name, id) {
+    this.name = name;
+    this.color = 'blue';
+    this.id = id;
+}
+
 /**
  * Views
  */
 
 app.get('/', function (req, res) {
-    res.json('Hello World!');
+    res.sendFile(__dirname + '/site/index.html');
+});
+
+
+/**
+ * Connection Management
+ */
+
+io.on('connection', function (socket) {
+    socket.on('new user', function (msg) {
+        var user = new User(msg, socket.id);
+        localUser = user;
+
+        console.log(JSON.stringify(user));
+        io.emit('created user', user);
+    });
+
+    socket.on('disconnect', function () {
+        io.emit('delete user', socket.id);
+    });
 });
 
 /**
